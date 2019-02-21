642014a7763b903cb394c792154a85371c6ea4ac
Rohini Senthil <rohinivsenthil@gmail.com>
2019-02-21 18:41:45 +0530

    Add bullet sprite

---
+++ bullet.py
@@ -0,0 +1,37 @@
+import math
+import json
+
+import pygame
+from pygame.locals import *
+
+
+with open('config.json') as configfile:
+    config = json.load(configfile)['bullet']
+
+SPEED = config['speed']
+
+
+bullet_surface = pygame.Surface((5, 5), SRCALPHA, 32)
+pygame.draw.circle(bullet_surface, pygame.Color(0, 0, 255), (2, 2), 2)
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, angle, pos):
+        super().__init__()
+
+        self.image = bullet_surface
+        self.rect = self.image.get_rect(center=pos)
+        self.mask = pygame.mask.from_surface(self.image)
+
+        self.pos = list(pos)
+
+        self.speed = [
+            math.sin(math.radians(angle)) * SPEED,
+            -math.cos(math.radians(angle)) * SPEED
+        ]
+
+    def update(self):
+        self.pos[0] += self.speed[0]
+        self.pos[1] += self.speed[1]
+
+        self.rect.center = self.pos
